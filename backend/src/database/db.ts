702d13b81cfc1f8adec60debb6fa1237ead481f2
salt-dev <salt-dev@salt-devs-MacBook-Pro-6.local>
2023-03-23 11:11:05 +0100

    Added connection to the database

---
+++ backend/src/database/db.ts
@@ -0,0 +1,7 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: 'postgres://ubuvsubq:OhSA6xVIBoefzurcipat--xJNwmHpjMB@trumpet.db.elephantsql.com/ubuvsubq',
+});
+
+export default pool;
---
+++ backend/src/database/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import pool from './db';
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+pool.connect();
+
+app.get('/api/students', (req, res) => {
+  pool.query('SELECT * FROM public."Products"')
+    .then(result => res.send(result.rows))
+    .catch(error => {
+      console.log(error);
+      res.send(500).send('An error occured while retrieving the Student data');
+    });
+});
+
+app.listen(port, () => console.log(`Running on http://localhost:${port}`));
