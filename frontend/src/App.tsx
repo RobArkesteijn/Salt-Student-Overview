2700dab9f705b32a15253da46a3ebb2f71f2a4ef
danchou-san <arianowong@gmail.com>
2023-03-28 13:05:34 +0200

    Finished Weekend Test Results page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.scss';
 import PrimarySearchAppBar from "./components/NavBar/NavBar";
+import TestResults from './components/TestResults/TestResults';
+import Login from './components/Login/Login';
 
 
 function App() {
-  return <PrimarySearchAppBar />;
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <TestResults />
+      {/* <Login/> */}
+    </>
+  )
 
 }
 
