d5aa325a56f0bbe0e645785ef24ab5eeecc89006
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-03-29 15:03:37 +0200

    Cleaned up unnecessary code

--- backend/app.ts
+++ backend/app.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import express from 'express';
 import pool from './database/db';
-import calendarRouter from './calendar';
 import cors from 'cors';
 
 
@@ -12,18 +11,15 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// pool.connect();
+pool.connect();
 
-// app.get('/api/students', (req, res) => {
-//   pool.query('SELECT * FROM public."Products"')
-//     .then(result => res.send(result.rows))
-//     .catch(error => {
-//       console.log(error);
-//       res.send(500).send('An error occurred while retrieving the Student data');
-//     });
-// });
-
-// Register the calendar router
-app.use('/', calendarRouter);
+app.get('/api/students', (req, res) => {
+  pool.query('SELECT * FROM public."Products"')
+    .then(result => res.send(result.rows))
+    .catch(error => {
+      console.log(error);
+      res.send(500).send('An error occurred while retrieving the Student data');
+    });
+});
 
 app.listen(port, () => console.log(`Running on http://localhost:${port}`));
